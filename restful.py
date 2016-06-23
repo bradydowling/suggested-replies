d14c8bbd07136d289a345fabcc9595db32403056
Brady Dowling <bradydowling@gmail.com>
2016-06-23 19:58:34 +0000

    Move saved replies to separate file

--- restful.py
+++ restful.py
@@ -2,17 +2,21 @@ import logging
 import os
 import requests
 import urllib
+import json
+from pprint import pprint
 from HTMLParser import HTMLParser
 from flask import Flask, jsonify, request
 from flask.ext.cors import CORS
-"""
-from pprint import pprint
+import talon
+from talon import quotations
+from nltk.stem.snowball import SnowballStemmer
 
-with open('saved_replies.json') as data_file:    
-    new_saved_replies = json.load(data_file)
+talon.init()
 
-pprint(new_saved_replies)
-"""
+with open('saved_replies.json') as json_data:
+    saved_replies = json.load(json_data)
+    json_data.close()
+
 app = Flask(__name__)
 CORS(app)
 
@@ -35,21 +39,6 @@ def strip_tags(html):
     return s.get_data()
 
 
-def getIntercomQuestion(conversation_id):
-    url = "https://api.intercom.io/conversations/" + conversation_id
-
-    headers = {
-        'accept': "application/json",
-        'authorization': "Basic MnljbHI5dmg6cm8tMzY2ZWM1ZWYxYTIyY2Q2MWQ4ODJkMzliY2E2MWEwY2NiOTBhZjYxMQ==",
-        'cache-control': "no-cache",
-    }
-
-    response = requests.request("GET", url, headers=headers)
-
-    user_question = strip_tags(response.json()['conversation_message']['body']).lower()
-
-    return user_question
-
 def getIntercomConversation(conversation_id):
     url = "https://api.intercom.io/conversations/" + conversation_id
 
@@ -65,8 +54,14 @@ def getIntercomConversation(conversation_id):
 
 def getConversationBody(conversation):
     user_question = strip_tags(conversation.json()['conversation_message']['body']).lower()
-
+    user_question = quotations.extract_from(user_question, 'text/plain')
+    user_question = quotations.extract_from_plain(user_question)
+    logging.warn(user_question)
     return user_question
+    
+def getConversationSubject(conversation):
+    user_question = getConversationBody(conversation)
+    
 
 def getIntercomUserID(conversation):
     user_id = conversation.json()['user']['id']
@@ -85,89 +80,6 @@ def getIntercomUser(user_id):
     user = requests.request("GET", url, headers=headers)
     
     return user
-
-saved_replies = [
-    {
-        'title': 'Blocked - First Response',
-        'keywords': ['agar', 'blocked', 'access my account', 'unblock'],
-    },
-    {
-        'title': 'Bug - Reported',
-        'keywords': ['ran into an issue', 'usersnap feedback', 'look into this'],
-    },
-    {
-        'title': 'Community Redirect (upsell)',
-        'keywords': ['ran into an issue', 'usersnap feedback', 'look into this'],
-    },
-    {
-        'title': 'Education',
-        'keywords': ['university', 'class', 'school', 'education'],
-    },
-    {
-        'title': 'FTP Issues',
-        'keywords': ['ftp', 'mount', 'sftp'],
-    },
-    {
-        'title': 'Invoices from Zuora',
-        'keywords': ['invoices'],
-    },
-    {
-        'title': 'Invoices from Zuora - Sent',
-        'keywords': ['invoices'],
-    },
-    {
-        'title': 'Job Applicant (Not hiring)',
-        'keywords': ['cv', 'resume', 'work at Cloud9']
-    },
-    {
-        'title': 'Monthly to yearly payments',
-        'keywords': ['monthly', 'year'],
-    },
-    {
-        'title': 'Red Alert Solved',
-        'keywords': ['unable to access your workspace']
-    },
-    {
-        'title': 'Refund - Downgrade First',
-        'keywords': ['refund', 'billed', 'thanks for your purchase with cloud9 ide']
-    },
-    {
-        'title': 'Refunded',
-        'keywords': ['refund', 'billed', 'thanks for your purchase with cloud9 ide']
-    },
-    {
-        'title': 'Share Email Reply',
-        'keywords': ['I\'ve shared a workspace']
-    },
-    {
-        'title': 'Solved (user reported)',
-        'keywords': ['seems to be good', 'solved', 'ignore this', 'all fine now'],
-    },
-    {
-        'title': 'Unstuck Workspace',
-        'keywords': ['access my workspace'],
-    },
-    {
-        'title': 'Unsubscribe',
-        'keywords': ['stop sending']
-    },
-    {
-        'title': 'Unsupported browser',
-        'keywords': ['internet explorer', 'ie', 'mobile', 'iphone', 'ipad', 'android', 'tablet'],
-    },
-    {
-        'title': 'Username change',
-        'keywords': ['change my username'],
-    },
-    {
-        'title': 'Workspace extra large',
-        'keywords': ['gb'],
-    },
-    {
-        'title': 'Yearly signup mistake',
-        'keywords': ['monthly', 'year'],
-    }
-]
 
 
 @app.route('/', methods=['GET'])
@@ -193,9 +105,9 @@ def get_suggested_replies(conversation_id):
 
     for reply in saved_replies:
         for keyword in reply['keywords']:
-            if (' ' + keyword) in user_question:
+            key = " " + keyword
+            if (key) in user_question:
                 replies_from_conversation.add(reply['title'])
-    logging.warn(replies_from_conversation)
     return jsonify({"matches": list(replies_from_conversation)})
 
 
@@ -204,7 +116,6 @@ def get_related_topics(conversation_id):
     conversation = getIntercomConversation(conversation_id)
     subject = conversation.json()['conversation_message']['subject']
     body = getConversationBody(conversation)
-    logging.warn(body)
     return jsonify({"subject": subject, "body": body})
 
 
