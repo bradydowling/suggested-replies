c409cfcf8d31cacd8908977286d2c12045aea89d
Brady Dowling <bradydowling@gmail.com>
2016-07-23 15:25:29 +0000

    Spam filter for most common spam type

--- helloworld/spam_filter.py
+++ helloworld/spam_filter.py
@@ -16,49 +16,48 @@ def convo_is_spam(convo_id):
     dom = BeautifulSoup(convo['conversation_message']['body'], 'html.parser')
     paragraphs = dom.find_all('p')
     links = dom.find_all('a')
-    print(dom.prettify())
     if convo['conversation_message']['author']['type'] is "admin":
-        print 'admin'
+        print convo_id + 'is not spam because it\'s from an admin'
         return False
     if not convo['conversation_message']['subject']:
-        print 'no subject'
+        print convo_id + 'is not spam because it has no subject'
         return False
     if len(paragraphs) < 2 or len(paragraphs) > 3:
-        print 'paragraph numbers mismatch'
+        print convo_id + 'is not spam because the paragraph numbers are off'
         return False
     if len(links) is not 2:
-        print 'links number mismatch'
+        print convo_id + 'is not spam because the number of links are off'
         return False
     if not paragraphs[-2].contents[-1]['href']:
-        print 'no second to last paragraph link'
+        print convo_id + 'is not spam because there\'s no link in the second to last paragraph'
         return False
     if not paragraphs[-1].contents[-1]['href']:
-        print 'no signature link'
+        print convo_id + 'is not spam because it has no link in the last paragraph'
         return False
-    print "it's spam"
+    print convo_id + " is spam"
     return is_spam
 
 
 def mark_spam(convo_id):
-    # "should assign to quick_close and assign tag 'spam' and block user?"
-    # quick_close = get quick close id
-    # convo.assign(quick_close)
-    print(intercom.makeNote(convo_id, "Automatically marked as spam"))
-    print(intercom.assignConversation(convo_id, QUICK_CLOSE))
+    # "should assign to quick_close and make not that it's spam"
+    intercom.makeNote(convo_id, "Filtered as spam")
+    intercom.assignConversation(convo_id, QUICK_CLOSE)
     return False
 
 
 def filter_all_open_convos():
+    spam_count = 0
     for admin in intercom.getAdmins()['admins']:
-        if admin['id'] is "174913":
+        if admin['type'] == "team":
             continue
         print(admin['name'])
         admin_convos = intercom.listConversations(admin['id'], True)
         for convo in admin_convos['conversations']:
             if convo_is_spam(convo['id']):
+                spam_count += 1
                 mark_spam(convo['id'])
 
-    return True
+    return spam_count
 
 
-mark_spam("5433906541")
+"Just filterd " + str(filter_all_open_convos()) + " spam conversations into Quick Close for you. You're welcome ;)"
